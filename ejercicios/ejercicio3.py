86faf20189e0bcce37302303f09db11e6168384e
schodiman <schodiman.h@gmail.com>
2025-07-03 23:02:20 -0400

    Update ejercicio3.py

--- ejercicios/ejercicio3.py
+++ ejercicios/ejercicio3.py
@@ -2,7 +2,23 @@
 # calcular la suma de esos números
 
 suma = 0
-for i in range(5):
-    numero = float(input('Ingrese número: '))
-    suma += numero
+numeros = []
+for indice in range(5):
+    # Suma manual de los valores ingresados
+    numero = 0
+    try:
+        numero = float(input('Ingrese número: '))
+        suma += numero
+        # Usar método sum() para sumar los elementos de la lista
+        numeros.append(numero)
+    except ValueError:
+        print('Valor ingresado NO corresponde')
+        suma += 0
+        numeros.append(0)
+    except:
+        print('Qué pasó con la aplicación??!!')
+
+# Imprimo la suma manual
 print(suma)
+# Imprimo sum()
+print(f"La suma de {numeros} = {sum(numeros)}")
