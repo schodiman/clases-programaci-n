460fc8a911ac74ceae0f44431cea8ce56e7293be
schodiman <schodiman.h@gmail.com>
2025-07-03 23:07:23 -0400

    Add files via upload

---
+++ ejercicios/ejercicio6.py
@@ -0,0 +1,14 @@
+# 1.- Escriba un programa que pida 3 números y los ordene de mayor a menor.
+
+# 2.- Escriba un programa que calcule el área y el perímetro de un cuadrado, 
+# si cualquier valor es negativo, se debe entregar un mensaje de error.
+
+# 3.- Escriba un programa que pida el precio de un producto y calcule el valor del IVA, 
+# si el precio es negativo, se debe entregar un mensaje de error.
+
+# 4.- Escriba un programa que permita calcular el área de un triángulo, 
+# si cualquier valor es negativo, se debe entregar un mensaje de error.
+
+# 5.- Escriba una calculadora básica que pida al usuario 2 números
+# y realice las 4 operaciones aritméticas básicas.
+# Si se trata de dividir por 0 se deberá entregar un mensaje de error.
